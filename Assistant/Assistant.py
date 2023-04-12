22ea393a3ee6eaaa6a185e72ee264da58d71935a
Samuel Liebert <Samuelliebert@gmail.com>
2023-04-11 21:57:34 -0500

    started wake word

--- Assistant/Assistant.py
+++ Assistant/Assistant.py
@@ -4,6 +4,7 @@ import inspect
 from Chat_Gpt import Chat_Gpt
 from Classes import Response
 from Decorators import paramRegistrar, reg
+from TTS import TTS
 
 
 class Assistant:
@@ -43,15 +44,59 @@ class Assistant:
                 possible_paramiters[skill_id].append(item)
 
         # chatbot
-        api_key = "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"
+        api_key = "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"
 
         self.action_functions = action_functions
         self.parameters = parameters
         self.possible_paramiters = possible_paramiters
         self.actions = actions
         self.chatbot = Chat_Gpt("Hal", api_key=api_key, actions=self.actions)
+        self.tts = TTS(lang="en-US")
+        self.punctuition = [".", ",", "?", "!", ":", ";", "\"", "'", "(", ")", "[", "]", "...", "-", "/", "\\", '"']
 
     def text_chat(self):
+        while True:
+            question = input("Q: ")
+
+            response = self.chatbot.ask(question)
+            message = ""
+
+            buffer = ""
+
+            json_identifier = "JS:"
+            is_json = None
+            
+            self.tts.start_phrases()
+
+            for i, chunk in enumerate(response):
+                if "content" in chunk["choices"][0]["delta"]:
+                    content = chunk["choices"][0]["delta"]["content"]
+                    message += content
+                    if json_identifier == "" and is_json == None:
+                        is_json = True
+                    elif is_json == None:
+                        for letter in content:
+                            if letter in json_identifier and json_identifier.index(letter) == 0:
+                                json_identifier = json_identifier.replace(
+                                    letter, "", 1)
+                            elif json_identifier != "":
+                                is_json = False
+                    if is_json == False and any(s in self.punctuition for s in content):
+                        if buffer:
+                            self.tts.say_phrase_in_phrases(buffer)
+                            buffer = ""
+                        self.tts.say_phrase_in_phrases(content)
+                    else:
+                        buffer += content
+
+            print(is_json)
+            self.tts.end_phrases()
+            file = open("logs.txt","w") 
+            file.write("Q: "+ question)
+            file.write("A: "+ content)
+
+
+    def text_to_voice_chat(self):
         while True:
             question = input("Q: ")
 
--- TTS/TTS.py
+++ TTS/TTS.py
@@ -1,25 +1,37 @@
 import pyttsx3
 
-test = "the quick brown fox"
-
 
 class TTS:
     def __init__(self, lang: str, voice_id="com.apple.speech.synthesis.voice.Alex"):
         engine = pyttsx3.init()
         engine.setProperty('voice', voice_id)
 
+        self.lang = lang
         self.engine = engine
 
-    def get_voices():
+    def get_voices(self):
         voices = self.engine.getProperty('voices')
 
-        engine.say(test)
-        engine.runAndWait()
         for voice in voices:
-            if "en_US" in voice.languages:
+            if self.lang in voice.languages:
                 print("Voice:")
                 print(" - ID: %s" % voice.id)
                 print(" - Name: %s" % voice.name)
                 print(" - Languages: %s" % voice.languages)
                 print(" - Gender: %s" % voice.gender)
                 print(" - Age: %s" % voice.age)
+
+    def say_phrase(self, phrase: str):
+        self.engine.say(phrase)
+        self.engine.runAndWait()
+    
+    def start_phrases(self):
+        self.engine.startLoop(False)
+
+    def end_phrases(self):
+        self.engine.endLoop()
+
+    
+    def say_phrase_in_phrases(self, phrase: str):
+        self.engine.say(phrase)
+        self.engine.iterate()
