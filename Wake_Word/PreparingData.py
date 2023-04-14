b03f7d5593ea220fdaafed17e5a34997d1375eeb
Sam Liebert <samliebert@sams-mbp.lan>
2023-04-13 21:05:45 -0500

    wake_word

--- Wake_Word/PreparingData.py
+++ Wake_Word/PreparingData.py
@@ -17,14 +17,17 @@ def record_audio_and_save(save_path, n_times=50):
     """
 
     input("To start recording Wake Word press Enter: ")
+    print()
     for i in range(n_times):
         fs = 44100
-        seconds = 2
 
-        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        seconds = 2
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
         sd.wait()
         write(save_path + str(i) + ".wav", fs, myrecording)
-        input(f"Press to record next or two stop press ctrl + C ({i + 1}/{n_times}): ")
+        input(
+            f"Press to record next or to stop press ctrl + C ({i + 1}/{n_times}): ")
+
 
 def record_background_sound(save_path, n_times=50):
     """
@@ -45,16 +48,17 @@ def record_background_sound(save_path, n_times=50):
     input("To start recording your background sounds press Enter: ")
     for i in range(n_times):
         fs = 44100
-        seconds = 2 
+        seconds = 2
 
-        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
         sd.wait()
         write(save_path + str(i) + ".wav", fs, myrecording)
         print(f"Currently on {i+1}/{n_times}")
 
+
 # Step 1: Record yourself saying the Wake Word
 print("Recording the Wake Word:\n")
-record_audio_and_save("audio_data/", n_times=100) 
+# record_audio_and_save("audio_data/", n_times=100)
 
 # Step 2: Record your background sounds (Just let it run, it will automatically record)
 print("Recording the Background sounds:\n")
--- Wake_Word/training.py
+++ Wake_Word/training.py
@@ -1,17 +1,16 @@
 ####### IMPORTS #############
 import numpy as np
 import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from sklearn.metrics import confusion_matrix, classification_report
-from plot_cm import plot_confusion_matrix
+from tensorflow.keras.layers import Activation, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
 
 ##### Loading saved csv ##############
 df = pd.read_pickle("final_audio_data_csv/audio_data.csv")
 
-####### Making our data training-ready
+# Making our data training-ready
 X = df["feature"].values
 X = np.concatenate(X, axis=0).reshape(len(X), 40)
 
@@ -19,7 +18,8 @@ y = np.array(df["class_label"].tolist())
 y = to_categorical(y)
 
 ####### train test split ############
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
 
 ##### Training ############
 
@@ -52,4 +52,3 @@ print("Model Classification Report: \n")
 y_pred = np.argmax(model.predict(X_test), axis=1)
 cm = confusion_matrix(np.argmax(y_test, axis=1), y_pred)
 print(classification_report(np.argmax(y_test, axis=1), y_pred))
-plot_confusion_matrix(cm, classes=["Does not have Wake Word", "Has Wake Word"])
