84bee2eab8c1f6dc811e8946c7dd5a95649c9250
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-16 11:20:10 -0500

    did some reordering

--- Hal/Voice/Voice.py
+++ Hal/Voice/Voice.py
@@ -45,57 +45,57 @@ class Voice:
         recognizer = sr.Recognizer()
         recognizer.energy_threshold = 3000
 
-        while True:
-            # Read PCM audio data
-            audio_frames = self.recorder.read()
+        with sr.Microphone(device_index=1) as source:
+            while True:
+                # Read PCM audio data
+                audio_frames = self.recorder.read()
 
-            # Process audio with Porcupine
-            keyword_index = self.porcupine.process(audio_frames)
+                # Process audio with Porcupine
+                keyword_index = self.porcupine.process(audio_frames)
 
-            if keyword_index >= 0:
-                print("Keyword detected")
+                if keyword_index >= 0:
+                    print("Keyword detected")
 
-                self.recorder.stop()
-                print(config["SR_MIC"], type(config["SR_MIC"]))
-                with sr.Microphone(device_index=1) as source:
+                    self.recorder.stop()
+
                     try:
                         # Capture speech input
-                        audio = recognizer.listen(
-                            source,
-                        )
-
+                        audio = recognizer.listen(source)
                         print("audio", audio, "type", type(audio))
                     except sr.UnknownValueError as e:
-                        print("unknown error occurred when trying to transcribe audio")
+                        print("Unknown error occurred when trying to transcribe audio")
                         threading.Thread(target=callback, args=("", e)).start()
-
+                        continue
                     except sr.RequestError as e:
                         print(
-                            e, "request error occurred when trying to transcribe audio"
+                            "Request error occurred when trying to transcribe audio:", e
                         )
                         threading.Thread(target=callback, args=("", e)).start()
-
+                        continue
                     except sr.WaitTimeoutError as e:
                         print(
+                            "Wait timeout error occurred when trying to transcribe audio:",
                             e,
-                            "wait timeout error occurred when trying to transcribe audio",
                         )
                         threading.Thread(target=callback, args=("", e)).start()
+                        continue
                     except Exception as e:
                         print(
+                            "A general error occurred when trying to transcribe audio:",
                             e,
-                            "A general error occurred when trying to transcribe audio",
                         )
                         threading.Thread(target=callback, args=("", e)).start()
-                try:
-                    # Use Google Speech Recognition to transcribe audio
-                    text = recognizer.recognize_google(audio)
-                    print("text", text)
-                    threading.Thread(target=callback, args=(text, None)).start()
-                except Exception as e:
-                    print(e)
-                    threading.Thread(target=callback, args=("", e)).start()
+                        continue
 
-                self.recorder.start()
-            else:
-                continue
+                    try:
+                        # Use Google Speech Recognition to transcribe audio
+                        text = recognizer.recognize_google(audio)
+                        print("text", text)
+                        threading.Thread(target=callback, args=(text, None)).start()
+                    except Exception as e:
+                        print(e)
+                        threading.Thread(target=callback, args=("", e)).start()
+
+                    self.recorder.start()
+                else:
+                    continue
