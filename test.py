1c22ceb59c5f39ecf7696790dd67700ba1005964
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-16 10:23:28 -0500

    more tests

--- test.py
+++ test.py
@@ -8,35 +8,45 @@ def list_microphones():
 
 
 list_microphones()
-input("Press Enter to continue...")
-# Create a recognizer object
-r = sr.Recognizer()
 
-# Define the microphone as the source
-mic = sr.Microphone(device_index=1)
 
-# Adjust the microphone sensitivity if needed
-mic.energy_threshold = 5000
+class ASR:
+    def start():
+        while True:
+            input("Press Enter to continue...")
+            # Create a recognizer object
+            r = sr.Recognizer()
 
-# Print the list of available microphones (optional)
-# print(sr.Microphone.list_microphone_names())
+            # Define the microphone as the source
+            mic = sr.Microphone(device_index=1)
 
-# Start recording from the microphone
-with mic as source:
-    print("Say something...")
-    audio = r.listen(source)
+            # Adjust the microphone sensitivity if needed
+            mic.energy_threshold = 5000
 
-try:
-    # Use Google Speech Recognition to transcribe the audio
-    text = r.recognize_google(audio)
-    print("Transcription: " + text)
+            # Print the list of available microphones (optional)
+            # print(sr.Microphone.list_microphone_names())
 
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
+            # Start recording from the microphone
+            with mic as source:
+                print("Say something...")
+                audio = r.listen(source)
 
-except sr.RequestError as e:
-    print(
-        "Could not request results from Google Speech Recognition service; {0}".format(
-            e
-        )
-    )
+            try:
+                # Use Google Speech Recognition to transcribe the audio
+                text = r.recognize_google(audio)
+                print("Transcription: " + text)
+
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+
+            except sr.RequestError as e:
+                print(
+                    "Could not request results from Google Speech Recognition service; {0}".format(
+                        e
+                    )
+                )
+
+
+asr = ASR()
+
+asr.start()
