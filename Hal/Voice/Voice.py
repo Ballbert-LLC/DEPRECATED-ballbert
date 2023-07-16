f36c2dfd518e9956c9584d18e50bdf5cbf39a493
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-16 10:07:07 -0500

    reordered code to maybe work better

--- Hal/Voice/Voice.py
+++ Hal/Voice/Voice.py
@@ -65,33 +65,37 @@ class Voice:
                         )
 
                         print("audio", audio, "type", type(audio))
-                        # Use Google Speech Recognition to transcribe audio
-                        text = recognizer.recognize_google(audio)
-                        print("text", text)
-                        # threading.Thread(target=callback, args=(text, None)).start()
-
                     except sr.UnknownValueError as e:
                         print("unknown error occurred when trying to transcribe audio")
-                        # threading.Thread(target=callback, args=("", e)).start()
+                        threading.Thread(target=callback, args=("", e)).start()
 
                     except sr.RequestError as e:
                         print(
                             e, "request error occurred when trying to transcribe audio"
                         )
-                        # threading.Thread(target=callback, args=("", e)).start()
+                        threading.Thread(target=callback, args=("", e)).start()
 
                     except sr.WaitTimeoutError as e:
                         print(
                             e,
                             "wait timeout error occurred when trying to transcribe audio",
                         )
-                        # threading.Thread(target=callback, args=("", e)).start()
+                        threading.Thread(target=callback, args=("", e)).start()
                     except Exception as e:
                         print(
                             e,
                             "A general error occurred when trying to transcribe audio",
                         )
-                        # threading.Thread(target=callback, args=("", e)).start()
+                        threading.Thread(target=callback, args=("", e)).start()
+                try:
+                    # Use Google Speech Recognition to transcribe audio
+                    text = recognizer.recognize_google(audio)
+                    print("text", text)
+                    threading.Thread(target=callback, args=(text, None)).start()
+                except Exception as e:
+                    print(e)
+                    threading.Thread(target=callback, args=("", e)).start()
+
                 self.recorder.start()
             else:
                 continue
