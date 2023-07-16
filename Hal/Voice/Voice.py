ff668930ab012b5c576c072cbd6ef7cc94f47196
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-16 11:01:24 -0500

    typo

--- Hal/Voice/Voice.py
+++ Hal/Voice/Voice.py
@@ -45,40 +45,39 @@ class Voice:
         recognizer = sr.Recognizer()
         recognizer.energy_threshold = 3000
 
-        def start():
-            while True:
-                input("Press Enter to continue...")
-                # Create a recognizer object
-                r = sr.Recognizer()
+        while True:
+            input("Press Enter to continue...")
+            # Create a recognizer object
+            r = sr.Recognizer()
 
-                # Define the microphone as the source
-                mic = sr.Microphone(device_index=1)
+            # Define the microphone as the source
+            mic = sr.Microphone(device_index=1)
 
-                # Adjust the microphone sensitivity if needed
-                mic.energy_threshold = 5000
+            # Adjust the microphone sensitivity if needed
+            mic.energy_threshold = 5000
 
-                # Print the list of available microphones (optional)
-                # print(sr.Microphone.list_microphone_names())
+            # Print the list of available microphones (optional)
+            # print(sr.Microphone.list_microphone_names())
 
-                # Start recording from the microphone
-                with mic as source:
-                    print("Say something...")
-                    audio = r.listen(source)
+            # Start recording from the microphone
+            with mic as source:
+                print("Say something...")
+                audio = r.listen(source)
 
-                try:
-                    # Use Google Speech Recognition to transcribe the audio
-                    text = r.recognize_google(audio)
-                    print("Transcription: " + text)
-                    callback(text, None)
+            try:
+                # Use Google Speech Recognition to transcribe the audio
+                text = r.recognize_google(audio)
+                print("Transcription: " + text)
+                callback(text, None)
 
-                except sr.UnknownValueError:
-                    print("Google Speech Recognition could not understand audio")
-                    callback("", "Google Speech Recognition could not understand audio")
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+                callback("", "Google Speech Recognition could not understand audio")
 
-                except sr.RequestError as e:
-                    print(
-                        "Could not request results from Google Speech Recognition service; {0}".format(
-                            e
-                        )
+            except sr.RequestError as e:
+                print(
+                    "Could not request results from Google Speech Recognition service; {0}".format(
+                        e
                     )
-                    callback("", e)
+                )
+                callback("", e)
