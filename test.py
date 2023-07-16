46545629d4bfb2b4bbc88e6769e19496c426ab17
Samuel Liebert <Samuelliebert@gmail.com>
2023-07-16 10:15:08 -0500

    testingh

--- test.py
+++ test.py
@@ -8,34 +8,35 @@ def list_microphones():
 
 
 list_microphones()
-# # Create a recognizer object
-# r = sr.Recognizer()
+input("Press Enter to continue...")
+# Create a recognizer object
+r = sr.Recognizer()
 
-# # Define the microphone as the source
-# mic = sr.Microphone(device_index=1)
+# Define the microphone as the source
+mic = sr.Microphone(device_index=1)
 
-# # Adjust the microphone sensitivity if needed
-# mic.energy_threshold = 5000
+# Adjust the microphone sensitivity if needed
+mic.energy_threshold = 5000
 
-# # Print the list of available microphones (optional)
-# # print(sr.Microphone.list_microphone_names())
+# Print the list of available microphones (optional)
+# print(sr.Microphone.list_microphone_names())
 
-# # Start recording from the microphone
-# with mic as source:
-#     print("Say something...")
-#     audio = r.listen(source)
+# Start recording from the microphone
+with mic as source:
+    print("Say something...")
+    audio = r.listen(source)
 
-# try:
-#     # Use Google Speech Recognition to transcribe the audio
-#     text = r.recognize_google(audio)
-#     print("Transcription: " + text)
+try:
+    # Use Google Speech Recognition to transcribe the audio
+    text = r.recognize_google(audio)
+    print("Transcription: " + text)
 
-# except sr.UnknownValueError:
-#     print("Google Speech Recognition could not understand audio")
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
 
-# except sr.RequestError as e:
-#     print(
-#         "Could not request results from Google Speech Recognition service; {0}".format(
-#             e
-#         )
-#     )
+except sr.RequestError as e:
+    print(
+        "Could not request results from Google Speech Recognition service; {0}".format(
+            e
+        )
+    )
